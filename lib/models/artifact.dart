7d2956a2d22333b1f9ce7e32e75ced16f2187d06
Pol Gimeno <pgimenolarrosa@gmail.com>
2023-04-25 19:08:50 +0200

    Model for Artifact class

---
+++ lib/models/artifact.dart
@@ -0,0 +1,33 @@
+class Artifact {
+  final int Id;
+  final int ImageNumber;
+  final int Points;
+  final double Latitude;
+  final double Longitude;
+
+  Artifact({
+    required this.Id,
+    required this.ImageNumber,
+    required this.Points,
+    required this.Latitude,
+    required this.Longitude,
+
+  });
+
+  factory Artifact.fromJson(Map<String, dynamic> json) {
+    return Artifact(
+      Id: json['Id'],
+      ImageNumber: json['ImageNumber'],
+      Points: json['Points'],
+      Latitude: json['Latitude'],
+      Longitude: json['Longitude']
+
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'Id': Id,
+    };
+  }
+}
