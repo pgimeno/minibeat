64529a1535419d30d39f7faf9531d686880bd9ab
jblanchg <systemjordi2@gmail.com>
2023-04-25 19:03:54 +0200

    Added new puzzle images

--- lib/screens/puzzle.dart
+++ lib/screens/puzzle.dart
@@ -38,7 +38,7 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
     // i fer updatePuzzle(llistaRebudaPerApi);
 
     //updatePuzzle([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
-    updatePuzzle([ 1, 4, 6, 7, 8, 12, 13, 15]);
+    updatePuzzle([2,3,4,5,7,8,9,10,11,12,15]);
   }
 
   @override
